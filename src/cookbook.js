1dcc33c82ecd108c6545b054b833430a9ee6f52b
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-12-04 10:43:32 -0500

    1. Adding recipes test passed

--- src/cookbook.js
+++ src/cookbook.js
@@ -21,3 +21,15 @@ class Cookbook {
 }
 
 module.exports = { Cookbook };
+
+
+// *** Just a test to see the code working for my easy reference later 
+//const myCookbook = new Cookbook();
+
+// console.log(`logging out the cookbook`)
+
+// myCookbook.addRecipe('Hotdog', ['mystery meat 1', 'bread', 'ketchup']);
+// myCookbook.addRecipe('Pizza', ['tomato', 'dough', 'cheese']);
+
+// console.log(myCookbook.listRecipes());
+// console.log(myCookbook.getRecipe('Pizza'));
--- test/cookbook.test.js
+++ test/cookbook.test.js
@@ -1,20 +1,32 @@
-const { Cookbook } = require('../src/cookbook');
+const { Cookbook } = require('../src/cookbook'); //we don't need to write ".js" if we are importing from one to another
 
 describe('Cookbook', () => {
   describe('Adding recipes', () => {
     test('should allow a recipe to be added', () => {
+      const myCookbook = new Cookbook();
+
+      myCookbook.addRecipe('cookie', ['egg', 'flour', 'sugar', 'chocolate']);
+       expect(myCookbook.recipes).toEqual({cookie: ['egg', 'flour', 'sugar', 'chocolate']});
+
+       //a test that fails (my ref):
+      // myCookbook.addRecipe("Toby's cookie", ['egg', 'flour', 'sugar', 'chocolate']);
+      // expect(myCookbook.recipes).toEqual({cookie: ['mystery dough', 'pecans']});
+
 
     });
   });
 
   describe('Listing recipes', () => {
     test('should allow the recipes to be listed', () => {
-
+ 
+        
     });
   });
 
   describe('Retrieving a recipe', () => {
     test('should allow the ingredients for a recipe to be retrieved', () => {
+      
+
 
     });
   });
